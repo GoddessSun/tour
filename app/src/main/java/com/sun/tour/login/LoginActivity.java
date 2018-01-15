4ba20526a717efd0d53919deb8ffa37391a58522
sunmingmao <1017090173@qq.com>
2018-01-15 14:53:08 +0800

    登录

--- app/src/main/java/com/sun/tour/login/LoginActivity.java
+++ app/src/main/java/com/sun/tour/login/LoginActivity.java
@@ -2,13 +2,19 @@ package com.sun.tour.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.sun.tour.R;
 import com.sun.tour.base.BaseActivity;
+
+import butterknife.BindView;
+
 @Route(path = "/tour/login/login_activity")
 public class LoginActivity extends BaseActivity {
 
+    @BindView(R.id.edit_phone)
+    EditText mPhoneEditView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
